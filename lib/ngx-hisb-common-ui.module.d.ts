972491ce76fdb8ef85eabc90c640d3b2b3466b5f
Plamen Tassev <plamen@gatech.edu>
2024-12-17 13:56:03 -0500

    Upgraded to ng19

--- lib/ngx-hisb-common-ui.module.d.ts
+++ lib/ngx-hisb-common-ui.module.d.ts
@@ -1,13 +1,9 @@
 import * as i0 from "@angular/core";
-import * as i1 from "@angular/material/icon";
-import * as i2 from "@angular/material/toolbar";
-import * as i3 from "@angular/router";
-import * as i4 from "@angular/material/divider";
-import * as i5 from "@angular/common";
-import * as i6 from "@angular/platform-browser/animations";
-import * as i7 from "@angular/material/button";
+import * as i1 from "@angular/router";
+import * as i2 from "@angular/common";
+import * as i3 from "@angular/platform-browser/animations";
 export declare class NgxHisbCommonUiModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<NgxHisbCommonUiModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<NgxHisbCommonUiModule, never, [typeof i1.MatIconModule, typeof i2.MatToolbarModule, typeof i3.RouterLink, typeof i4.MatDividerModule, typeof i5.NgIf, typeof i6.BrowserAnimationsModule, typeof i7.MatButtonModule], never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<NgxHisbCommonUiModule, never, [typeof i1.RouterLink, typeof i2.NgIf, typeof i3.BrowserAnimationsModule], never>;
     static ɵinj: i0.ɵɵInjectorDeclaration<NgxHisbCommonUiModule>;
 }
