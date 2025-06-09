91df7487cddb7b7602c74fd1b64f9acc7d0b5ce2
ptassev3 <plamentassev@gmail.com>
2025-06-09 11:26:05 -0400

    updated to ng20

--- lib/ngx-hisb-common-ui.module.d.ts
+++
@@ -1,9 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "@angular/router";
-import * as i2 from "@angular/common";
-import * as i3 from "@angular/platform-browser/animations";
-export declare class NgxHisbCommonUiModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<NgxHisbCommonUiModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<NgxHisbCommonUiModule, never, [typeof i1.RouterLink, typeof i2.NgIf, typeof i3.BrowserAnimationsModule], never>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<NgxHisbCommonUiModule>;
-}
